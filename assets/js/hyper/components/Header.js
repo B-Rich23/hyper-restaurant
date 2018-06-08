93cbeaf6a9b0bf67f371e9ac3a762f0249afb3bf
Brandan Richardson <brandanr@gmail.com>
2018-06-07 23:07:04 -0700

    first commit

---
+++ assets/js/hyper/components/Header.js
@@ -0,0 +1,19 @@
+import {h, app} from 'hyperapp'
+
+export default function Header({state, actions}) {
+  return (
+    <header>
+      <div className="logo">
+        Logo
+      </div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">Party Platters</a>
+        <a href="#">Locations</a>
+        <a href="#">Rewards</a>
+        <a href="#">Reservations</a>
+      </nav>
+    </header>
+  )
+}
