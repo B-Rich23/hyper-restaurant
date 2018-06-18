88cd37ce7074194e76dc40a80b3787c78706db44
Brandan Richardson <brandanr@gmail.com>
2018-06-18 11:47:51 -0700

    added data for Reviews component slider in globalState.js

--- assets/js/hyper/state/globalState.js
+++ assets/js/hyper/state/globalState.js
@@ -24,10 +24,49 @@ let menu = [
   }
 ]
 
+let reviews = [
+  {
+    company: 'CHEF MASTERS',
+    highlight: 'A breathtaking steakhouse experience!',
+    author: 'Rich Belini',
+    authorInfo: '- winner of Chef Masters',
+    review: 'Lorem ipsum dolor amet salvia keytar disrupt etsy cloud bread before they sold out kombucha unicorn chartreuse hexagon single-origin coffee paleo beard fixie taxidermy. Shoreditch affogato neutra brooklyn, food truck polaroid irony lomo narwhal. Marfa normcore 8-bit wayfarers kickstarter ethical XOXO tousled wolf keytar offal ugh.'
+  },
+  {
+    company: 'TOP COOK',
+    highlight: 'Delectable delights abound!',
+    author: 'Sasha Murphy',
+    authorInfo: '- host of Top Cook',
+    review: 'Lorem ipsum dolor amet salvia keytar disrupt etsy cloud bread before they sold out kombucha unicorn chartreuse hexagon single-origin coffee paleo beard fixie taxidermy. Shoreditch affogato neutra brooklyn, food truck polaroid irony lomo narwhal. Marfa normcore 8-bit wayfarers kickstarter ethical XOXO tousled wolf keytar offal ugh.'
+  },
+  {
+    company: 'KITCHEN WARS',
+    highlight: 'Must stop steak shop!',
+    author: 'Terry Sanders',
+    authorInfo: '- producer of Kitchen Wars',
+    review: 'Lorem ipsum dolor amet salvia keytar disrupt etsy cloud bread before they sold out kombucha unicorn chartreuse hexagon single-origin coffee paleo beard fixie taxidermy. Shoreditch affogato neutra brooklyn, food truck polaroid irony lomo narwhal. Marfa normcore 8-bit wayfarers kickstarter ethical XOXO tousled wolf keytar offal ugh.'
+  },
+  {
+    company: 'THE FOODIE CHANNEL',
+    highlight: 'A steak experience par excellence!',
+    author: 'Katrina Oliver',
+    authorInfo: '- executive chef on The Foodie Channel',
+    review: 'Lorem ipsum dolor amet salvia keytar disrupt etsy cloud bread before they sold out kombucha unicorn chartreuse hexagon single-origin coffee paleo beard fixie taxidermy. Shoreditch affogato neutra brooklyn, food truck polaroid irony lomo narwhal. Marfa normcore 8-bit wayfarers kickstarter ethical XOXO tousled wolf keytar offal ugh.'
+  },
+  {
+    company: 'CHEF GLADIATORS',
+    highlight: 'Your taste buds will thank me!',
+    author: 'Hank Espinoza',
+    authorInfo: '- reigning gladiator on Chef Gladiators',
+    review: 'Lorem ipsum dolor amet salvia keytar disrupt etsy cloud bread before they sold out kombucha unicorn chartreuse hexagon single-origin coffee paleo beard fixie taxidermy. Shoreditch affogato neutra brooklyn, food truck polaroid irony lomo narwhal. Marfa normcore 8-bit wayfarers kickstarter ethical XOXO tousled wolf keytar offal ugh.'
+  }
+]
+
 
 export const globalState = {
   count: 0,
   companyInfo,
-  menu
+  menu,
+  reviews
 }
 
